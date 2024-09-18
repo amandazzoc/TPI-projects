170aebccbff2e40acc29142410a5e56b9a0e0175
DSM2 <fatec-dsm2@academico.intra>
2024-09-17 22:27:34 -0300

    commit

---
+++ projeto/aula_java.sql
@@ -0,0 +1,11 @@
+create database aula_java;
+
+use aula_java;
+
+create table Cliente(
+	id int(4) auto_increment primary key,
+    codigo int(5) not null,
+    nome varchar(80),
+    telefone varchar(11),
+    email varchar(120)
+);
